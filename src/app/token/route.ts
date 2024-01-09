139e2a690d3680dc68f9be74cda567f0a93a6fa2
vrajdesai78 <vrajdesai78@gmail.com>
2024-01-09 23:29:15 +0530

    optimized code to generate accessToken

--- src/app/token/route.ts
+++ src/app/token/route.ts
@@ -3,40 +3,6 @@ import * as jose from "jose";
 
 export const dynamic = "force-dynamic";
 
-const createToken = async (
-  roomId: string,
-  role: string,
-  displayName: string
-) => {
-  const accessToken = new AccessToken({
-    apiKey: process.env.API_KEY!,
-    roomId: roomId as string,
-    role: role,
-    permissions: {
-      admin: true,
-      canConsume: true,
-      canProduce: true,
-      canProduceSources: {
-        cam: true,
-        mic: true,
-        screen: true,
-      },
-      canRecvData: true,
-      canSendData: true,
-      canUpdateMetadata: true,
-    },
-    options: {
-      metadata: {
-        displayName: displayName,
-      },
-    },
-  });
-
-  const token = await accessToken.toJwt();
-
-  return token;
-};
-
 export async function POST(request: Request) {
   const { roomId, appPubKey } = await request.json();
   const idToken = request.headers.get("authorization")?.split(" ")[1];
@@ -53,17 +19,33 @@ export async function POST(request: Request) {
     algorithms: ["ES256"],
   });
 
-  let token: string;
-
   if ((jwtDecoded.payload as any).wallets[0].public_key === appPubKey) {
-    token = await createToken(
-      roomId,
-      Role.HOST,
-      (jwtDecoded.payload as any).name
-    );
+    const accessToken = new AccessToken({
+      apiKey: process.env.API_KEY!,
+      roomId: roomId as string,
+      role: Role.HOST,
+      permissions: {
+        admin: true,
+        canConsume: true,
+        canProduce: true,
+        canProduceSources: {
+          cam: true,
+          mic: true,
+          screen: true,
+        },
+        canRecvData: true,
+        canSendData: true,
+        canUpdateMetadata: true,
+      },
+      options: {
+        metadata: {
+          displayName: (jwtDecoded.payload as any).name,
+        },
+      },
+    });
+    const token = await accessToken.toJwt();
+    return new Response(token, { status: 200 });
   } else {
     return new Response("Unauthorized", { status: 401 });
   }
-
-  return new Response(token, { status: 200 });
 }
